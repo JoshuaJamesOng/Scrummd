041466063f212ac464d58e034fcfa62837f953be
Joshua Ong <ong.joshuajames@gmail.com>
2015-07-24 20:56:42 +0100

    Factory

--- lib/src/main/java/com/ongtonnesoup/scrummd/domain/factorys/ColourThemeFactory.java
+++ lib/src/main/java/com/ongtonnesoup/scrummd/domain/factorys/ColourThemeFactory.java
@@ -1,15 +1,15 @@
-package com.ongtonnesoup.scrummd.domain.builders;
+package com.ongtonnesoup.scrummd.domain.factorys;
 
 import com.ongtonnesoup.scrummd.domain.models.theme.ColourTheme;
 
-public class ColourThemeBuilder<T> {
+public class ColourThemeFactory<T> {
 
-    public ColourTheme<T> generateNewColourTheme(T primary, T secondary, T accent) {
+    public static <T> ColourTheme<T> createColourTheme(T primary, T secondary, T accent) {
         ColourTheme<T> theme = new ColourTheme<>();
         theme.setPrimary(primary);
         theme.setSecondary(secondary);
         theme.setAccent(accent);
         return theme;
     }
-    
+
 }
