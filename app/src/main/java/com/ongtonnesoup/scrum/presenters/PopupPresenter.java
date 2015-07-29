4e7d43bd4ce718ec5e3f4d18411fb6bfb5cbf3b1
Joshua Ong <ong.joshuajames@gmail.com>
2015-07-29 18:57:48 +0100

    Created presenter and view for popup

---
+++ app/src/main/java/com/ongtonnesoup/scrum/presenters/PopupPresenter.java
@@ -0,0 +1,12 @@
+package com.ongtonnesoup.scrum.presenters;
+
+import com.ongtonnesoup.scrum.views.PopupView;
+
+public class PopupPresenter {
+
+    private final PopupView mView;
+
+    public PopupPresenter(PopupView view) {
+        mView = view;
+    }
+}
---
+++ app/src/main/java/com/ongtonnesoup/scrum/views/PopupView.java
@@ -0,0 +1,7 @@
+package com.ongtonnesoup.scrum.views;
+
+public interface PopupView {
+
+    void closePopup();
+
+}
